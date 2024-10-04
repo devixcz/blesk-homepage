49cdedeba8baa3671640cfda34044cd926aef2d0
Petr Konůpek <petr.konupek@devix.cz>
2024-10-04 16:05:06 +0200

    Color theme

--- app/theme.ts
+++ app/theme.ts
@@ -1,10 +1,22 @@
 'use client';
 import { createTheme } from '@mui/material/styles';
+import { ThemeOptions } from '@mui/material/styles';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: 'var(--font-roboto)',
+const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#ed1c1d',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#e9eff4',
+    },
   },
-});
+};
+
+const theme = createTheme(themeOptions);
 
 export default theme;
