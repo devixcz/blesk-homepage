259e2a82dc4a7974f5b8921eb37bc102de3fc1c0
Petr Konůpek <petr.konupek@devix.cz>
2024-10-31 18:48:02 +0100

    Removing  storybook addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,12 +7,10 @@ const config: StorybookConfig = {
     "../**/*.stories.@(js|jsx|mjs|ts|tsx)", // Hledá soubory .stories v adresářích komponent
   ],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    "@storybook/addon-docs",
   ],
   framework: {
     name: "@storybook/nextjs",
